030c5025982717e1221baaf78aa70973d2314e85
Suraj Sanwal <sonusanwal65@gmail.com>
2018-03-13 12:14:29 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import Router from './components/myRoutes';
+
+export default class App extends Component {
+      render() {
+            return (
+                  <Router />
+            )
+      }
+}
---
+++ src/myRoutes.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Stack, Scene, Router, ActionConst, Drawer } from 'react-native-router-flux';
+// import sidebarComponent from './components/sidebar'
+// import Home from './components/home'
+import icon from './img/logo.webp'
+
+export default class MyRouter extends Component {
+      constructor(props) {
+            super(props)
+      }
+
+      render() {
+            return (
+                  <View>
+                        <Text>Hello</Text>
+                  </View>
+            )
+      }
+}
